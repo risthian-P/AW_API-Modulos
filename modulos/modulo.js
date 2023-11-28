6cf5cdf61e2214ebfe52579b70bdb558a961c075
CRISTHIAN PAÑORA <117744044+risthian-P@users.noreply.github.com>
2023-11-28 00:49:05 -0500

    Add files via upload

---
+++ modulos/modulo.js
@@ -0,0 +1,31 @@
+// login = (user,pass) => {
+//     if(!user && pass) return conosle.log("No autorizado")
+//     console.log(`Welcome ${user}`)
+// }
+
+// const LIKES = 500
+//commonjs
+//module.exports = login
+// module.exports = {
+//     login,
+//     LIKES
+// }
+
+//es modules
+// export default login // un solo dato
+
+// export{
+//     login,
+//     LIKES
+// }
+
+
+const conFe = () => { console.log("Fecha coninvertida") }
+const conMo= () => { console.log("Moneda convertida") }
+const conBase = () => "conexion*/300/user/password"
+
+export{
+       conFe,
+       conMo,
+       conBase
+    }
